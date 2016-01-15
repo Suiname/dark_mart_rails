10efcfbb188f20cfd6a1e9712188aae7b728163a
Jason Tham <jtham@sbcglobal.net>
2016-01-15 12:32:26 -0600

    all stuff

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+
+  before_action :authorize
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      head :no_content
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @products = Product.all
+    render json: @products, status: :ok
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    if @product
+      render json: @product, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product
+      @product.destroy
+      head :no_content
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def product_params
+    params.permit(:name, :description, :price)
+  end
+end
